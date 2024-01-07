239a1d653e7aaccaa0ca29dd5aad24a94339ba39
miha53cevic <44951515+miha53cevic@users.noreply.github.com>
2024-01-07 17:09:32 +0100

    finish lasso select

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 - [Flood Fill](https://miha53cevic.github.io/PaintAlgorithms/visualisations/FloodFill/)
 - [Bezier Curves](https://miha53cevic.github.io/PaintAlgorithms/visualisations/BezierCurves/)
 - [Spline Curves](https://miha53cevic.github.io/PaintAlgorithms/visualisations/SplineCurves/)
-- [PaintBrush](https://miha53cevic.github.io/PaintAlgorithms/visualisations/PaintBrush/)
+- [Paint Brush](https://miha53cevic.github.io/PaintAlgorithms/visualisations/PaintBrush/)
 - [Color Picker](https://miha53cevic.github.io/PaintAlgorithms/visualisations/ColorPicker/)
+- [Lasso Select](https://miha53cevic.github.io/PaintAlgorithms/visualisations/LassoSelect/)
--- visualisations/LassoSelect/main.js
+++ visualisations/LassoSelect/main.js
@@ -16,20 +16,62 @@ class Rect {
      * @param {Point} p topLeftCorner
      * @param {Number} w Width
      * @param {Number} h Height
+     * @param {Boolean} selected isSelected
      */
-    constructor(p, w, h) {
+    constructor(p, w, h, selected = false) {
         this.p = p;
         this.w = w;
         this.h = h;
+        this.selected = selected;
     }
 }
 
-function LineSegmentsIntersection(p0, p1, p2, p3) {
-    
+/**
+ * @param {Point} p0 
+ * @param {Point} p1 
+ * @param {Point} p2 
+ * @param {Point} p3 
+ */
+function LineSegmentsIntersection(p1, q1, p2, q2) {
+    function Orientation(p, q, r) {
+        const val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+        if (val === 0) return 0; // Collinear
+        else return (val > 0) ? 1 : 2; // Clockwise or counterclockwise
+    }
+
+    function OnSegment(p, q, r) {
+        return q.x <= Math.max(p.x, r.x) && q.x >= Math.min(p.x, r.x) && q.y <= Math.max(p.y, r.y) && q.y >= Math.min(p.y, r.y);
+    }
+
+    const o1 = Orientation(p1, q1, p2)
+    const o2 = Orientation(p1, q1, q2)
+    const o3 = Orientation(p2, q2, p1)
+    const o4 = Orientation(p2, q2, q1)
+
+    if (o1 != o2 && o3 != o4) return true;
+    if (o1 == 0 && OnSegment(p1, p2, q1)) return true;
+    if (o2 == 0 && OnSegment(p1, q2, q1)) return true;
+    if (o3 == 0 && OnSegment(p2, p1, q2)) return true;
+    if (o4 == 0 && OnSegment(p2, q1, q2)) return true;
+
+    return false;
 }
 
+/**
+ * @param {Point} p0 
+ * @param {Point} p1 
+ * @param {Rect} rect 
+ */
 function LineSegmentRectIntersect(p0, p1, rect) {
-
+    const topLeft = rect.p;
+    const topRight = new Point(rect.p.x + rect.w, rect.p.y);
+    const bottomLeft = new Point(rect.p.x, rect.p.y + rect.h);
+    const bottomRight = new Point(rect.p.x + rect.w, rect.p.y + rect.h);
+    const a1 = LineSegmentsIntersection(p0, p1, topLeft, topRight);
+    const a2 = LineSegmentsIntersection(p0, p1, topLeft, bottomLeft);
+    const a3 = LineSegmentsIntersection(p0, p1, bottomLeft, bottomRight);
+    const a4 = LineSegmentsIntersection(p0, p1, topRight, bottomRight);
+    return Math.max(a1, a2, a3, a4) === 1 ? true : false;
 }
 
 /**
@@ -70,10 +112,70 @@ function RectInPolygon(path, rect) {
         p0 = p1;
     }
     if (counter % 2 !== 0) return true;
-    return RectIntersectsPath(path, rectangle);
+    return RectIntersectsPath(path, rect);
 };
 
+let path = [];
+let rects = [];
 function Init() {
+
+    let holding = false;
+    ctx.canvas.addEventListener('mousedown', (e) => {
+        if (e.which !== 1) return;
+        holding = true;
+        path = []; // reset path array
+        // Deselect previous selected
+        for (const rect of rects) {
+            rect.selected = false;
+        }
+    });
+
+    ctx.canvas.addEventListener('mouseup', (e) => {
+        if (e.which !== 1) return;
+        holding = false;
+
+        if (path.length == 0) return; // if only left click is pressed and released withouth movement after
+
+        // Add first point again so it's a closed loop
+        path.push(path[0]);
+
+        console.log("Path: ", path);
+
+        // Run algorithm
+        for (const rect of rects) {
+            const selected = RectInPolygon(path, rect);
+            rect.selected = selected;
+        }
+    });
+
+    ctx.canvas.addEventListener('mousemove', () => {
+        if (!holding) return;
+        // Add points to path
+        path.push(new Point(Math.floor(MOUSE_POS.x), Math.floor(MOUSE_POS.y)));
+    });
+
+    rects.push(new Rect(new Point(100, 100), 50, 50));
+    rects.push(new Rect(new Point(150, 233), 75, 50));
+    rects.push(new Rect(new Point(400, 330), 50, 120));
+    rects.push(new Rect(new Point(400, 75), 200, 45));
+
+    requestAnimationFrame(Loop);
+}
+
+function Loop() {
+    clear('white');
+
+    // Draw rects
+    for (const rect of rects) {
+        const colour = rect.selected ? 'yellow' : 'greenyellow';
+        drawFillRect(rect.p.x, rect.p.y, rect.w, rect.h, colour)
+    }
+
+    // Draw path
+    if (path.length > 0)
+        lineStrip(path, 'black');
+
+    requestAnimationFrame(Loop);
 }
 
 Init();
